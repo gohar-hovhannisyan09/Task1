87d897148f61d133c5e7867e567c4163fda3e00c
gohar-hovhannisyan09 <gohar.hovhannisyan09@gmail.com>
2025-01-30 21:42:29 +0400

    Реализована соединения с базой данных

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,27 @@
 package jm.task.core.jdbc.util;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Util {
     // реализуйте настройку соеденения с БД
+    private static final String URL = "jdbc:postgresql://localhost:5432/users";
+    private static final String USERNAME = "postgres";
+    private static final String PASSWORD = "Sedsam0709***";
+
+    static {
+
+        try {
+            Class.forName("org.postgresql.Driver");
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException("Не удалось загрузить драйвер PostgreSQL", e);
+        }
+
+        try (Connection connection = DriverManager.getConnection(URL, USERNAME, PASSWORD)) {
+
+        } catch (SQLException e) {
+            throw new RuntimeException("Ошибка подключения к базе данных", e);
+        }
+    }
 }
