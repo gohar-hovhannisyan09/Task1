7ad092d826b8bac6a37ce19adff8bd78218231de
gohar-hovhannisyan09 <gohar.hovhannisyan09@gmail.com>
2025-02-01 23:34:47 +0400

    Реализация для таблицы пользователья с postgreSQL

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -6,7 +6,7 @@ import java.sql.SQLException;
 
 public class Util {
     // реализуйте настройку соеденения с БД
-    private static final String URL = "jdbc:postgresql://localhost:5432/users";
+    private static final String URL = "jdbc:postgresql://localhost:5432/postgres";
     private static final String USERNAME = "postgres";
     private static final String PASSWORD = "Sedsam0709***";
 
